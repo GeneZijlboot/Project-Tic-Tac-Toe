61d7685c7aa44fc361f61b372be2446dbe9c31de
GeneZijlboot <gene.cyberzijlmans@gmail.com>
2023-06-28 12:07:24 +0200

    console.log the array information

---
+++ script.js
@@ -0,0 +1,10 @@
+const marks = ["X", "O", "X", "O", "X", "O", "X", "O", "X"];
+
+function Render(){
+    for(let i = 0; i < marks.length; i++){
+        let element = marks[i];
+        console.log(element);
+    }
+}
+
+Render();
