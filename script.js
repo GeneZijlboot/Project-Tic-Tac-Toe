e52c2fca5da96d5b751daa1eebd78b489cc348fd
GeneZijlboot <gene.cyberzijlmans@gmail.com>
2023-06-29 17:29:54 +0200

    displaying 'X' on a specific tile and adding it to the correct array[item]

--- script.js
+++ script.js
@@ -1,36 +1,54 @@
 //the array for the game itself
-const marks = ["X", "O", "X", "O", "X", "O", "X", "O", "X"];
+const marks = ["X" ];
+const Moves = [0, 1, 2, 3, 4, 5, 6, 7, 8];
 
-//declaring variables 
-const buttons = document.querySelectorAll('.buttons');
-const ClearBtn = document.querySelector('.ClearBtn');
-
-//factory function for Player1 and Player2
+//factory function for player1 and player2
 const Players = (name, marker) => {
-    const logem = () => console.log(name, marker); //console.logs the outputs of PLayer 1 and 2
-    return { name, marker, logem};
+    return { name, marker};
 }
 
 const Player1 = Players('player1', 'X');
 const Player2 = Players('player2', 'O');
-Player1.logem();
-Player2.logem();
 
-buttons.forEach((buttons) => buttons.addEventListener('click', function() {
-    Render();
-}))
+const buttons = document.querySelectorAll('.buttons');
+const ClearBtn = document.querySelector('.ClearBtn');
 
-//this functions render the array contents to the webpage
+//render the array contents to the DOM
 function Render(){
     for(let i = 0; i < marks.length; i++){
         buttons[i].textContent = marks[i];
     }
 }
 
+buttons.forEach((buttonplace) => buttonplace.addEventListener('click', function() {
+    marks.push(buttonplace.textContent = Player1.marker); 
+    console.log(buttonplace);
+    console.log(marks);
+}))
+
 //Clears Playeing Field
 ClearBtn.addEventListener('click', () => {
     while (marks.length > 0) {
-        marks.pop(); //makes the array empty
+        marks.pop(); //pop() --> deletes everything inside a give (marks) array
     }
     buttons.forEach((buttons) => buttons.textContent = " ");
 })
+
+function PlayersTurn(){
+    for(let i = 0; i < Moves.length; i++){
+        if(i === 0 || i === 2 || i === 4 || i === 6 || i === 8){
+            console.log(i);
+            console.log('even numbers, so that would be Player1Marker = X');
+        } else if(i === 1 || i === 3 || i === 5 || i === 7){
+            console.log(i);
+            console.log('odd numbers, so that would be Player2Marker = O');
+        }
+    }
+}
+
+//========= for the winning player! ========//
+function GameOver(winningplayer){
+    console.log("Congratulations, " + winningplayer + " you have won!");
+}
+
+GameOver(Player1.name);
