8bc9302cd967a9d2eb11d036b53359547b982540
GeneZijlboot <gene.cyberzijlmans@gmail.com>
2023-07-03 22:23:17 +0200

    making code clean/read-able --> only thing left is the winningplayerfunction

--- script.js
+++ script.js
@@ -1,79 +1,85 @@
-//the array for the game itself
+//declaring variables
+const AllTiles = document.querySelectorAll('.Tiles');
+const CurrentPlayer = document.querySelector('.SwapPlayer');
+const ClearBtn = document.querySelector('.ClearBtn');
+
 let marks = ["", "", "", "", "", "", "", "", ""];
-let PlayerTurnOne = true;
-let PlayerTurnTwo = false;
-//factory function for player1 and player2
+const winConditions = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+];
+
+//factory function for the player.name and player.marker
 const Players = (name, marker) => {
     return { name, marker};
 }
+const Player1 = Players('Player 1', 'X');
+const Player2 = Players('Player 2', 'O');
 
-const Player1 = Players('player1', 'X');
-const Player2 = Players('player2', 'O');
+let PlayerTurnOne = true;
+let PlayerTurnTwo = false;
 
-const AllTiles = document.querySelectorAll('.Tiles');
-const ClearBtn = document.querySelector('.ClearBtn');
-const SwapPlayer = document.querySelector('.SwapPlayer');
+//when clicking a specific tile --> swap the players and put player.mark down
+const CellClicked = (() => {
+    AllTiles.forEach((SpecificTile) => SpecificTile.addEventListener('click', SwapPlayer));
+    AllTiles.forEach((SpecificTile) => SpecificTile.addEventListener('click', () =>{
+        SpecificTile.disabled = true; 
+        ClearBtn.addEventListener('click', () => {
+            SpecificTile.disabled = false;
+        })  
+    }));
+})();
 
-//render the array contents to the DOM
+//Renders evrything in the Array to the Dom
 function Render(){
     for(let i = 0; i < marks.length; i++){
         AllTiles[i].textContent = marks[i];
     }
 }
 
-AllTiles.forEach((SpecificTile) => SpecificTile.addEventListener('click', function() {
+//Swaps the player.name and player.marker
+function SwapPlayer(){
     const TileIndex = this.getAttribute("TileIndex");
-    //deciding whos turn it is, and switch it after clicking
     if(PlayerTurnOne === true || PlayerTurnTwo === false){
         PlayerTurnOne = false;
         PlayerTurnTwo = true;
 
         marks[TileIndex] = Player1.marker;
-        console.log(marks);
+        CurrentPlayer.textContent = Player2.name;
+
         Render();
+        Checkwinner();
 
-        //swap players visuals
-        SwapPlayer.textContent = "Player 2";
-
-        //disable tile after clicking
-        SpecificTile.disabled = true;
-
-        //after clicking clear button, tiles are available again
-        ClearBtn.addEventListener('click', () => {
-            SpecificTile.disabled = false;
-        })
-    //deciding whos turn it is, and switch it after clicking
-    } else if( PlayerTurnOne === false || PlayerTurnTwo === true){
+    }else if(PlayerTurnOne === false || PlayerTurnTwo === true){
         PlayerTurnOne = true;
         PlayerTurnTwo = false;
 
         marks[TileIndex] = Player2.marker;
-        console.log(marks);
+        CurrentPlayer.textContent = Player1.name;
+
         Render();
-
-        //swap players visuals
-        SwapPlayer.textContent = "Player 1";
-
-        //disable tile after clicking
-        SpecificTile.disabled = true;
-
-        //after clicking clear button, tiles are available again
-        ClearBtn.addEventListener('click', () => {
-            SpecificTile.disabled = false;
-        })
+        Checkwinner();
     }
+}
 
-    
-}))
+//Checks who Wins
+function Checkwinner(){
 
-//Clears Playeing Field
-ClearBtn.addEventListener('click', () => {
-    while (marks.length > 0) {
-        marks.pop(); //pop() --> deletes everything inside a give (marks) array
-    }
-    AllTiles.forEach((SpecificTile) => SpecificTile.textContent = "");
-    SwapPlayer.textContent = "Player 1";
-    PlayerTurnOne = true;
-    PlayerTurnTwo = false;
-    AllTiles.disabled = false;
-})
+}
+
+//Restart button
+const ClearField = (() => {
+    ClearBtn.addEventListener('click', () => {
+        marks = ["", "", "", "", "", "", "", "", ""];
+        AllTiles.forEach((SpecificTile) => SpecificTile.textContent = "");
+        CurrentPlayer.textContent = Player1.name;
+        PlayerTurnOne = true;
+        PlayerTurnTwo = false;
+    })
+})();
